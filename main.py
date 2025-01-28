cd47f70da71b2023c3dd13a82c171db432e56285
vanillachan-vs-theworld <ldaelnismine@gmail.com>
2025-01-27 22:57:46 -0600

    Create HTML website

---
+++ main.py
@@ -0,0 +1,72 @@
+from flask import Flask,request,render_template,redirect
+import os
+app = Flask(__name__)
+import cv2
+import numpy as np
+
+app.config["IMAGE_UPLOADS"] = "Image_Upload"
+#app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG","JPG","JPEG"]
+
+from werkzeug.utils import secure_filename
+
+
+@app.route('/home',methods = ["GET","POST"])
+def upload_image():
+	if request.method == "POST":
+		image = request.files['file']
+
+		if image.filename == '':
+			print("Image must have a file name")
+			return redirect(request.url)
+
+
+		filename = secure_filename(image.filename)
+
+		basedir = os.path.abspath(os.path.dirname(__file__))
+		image.save(os.path.join(basedir,app.config["IMAGE_UPLOADS"],filename))
+
+		return render_template("main.html",filename=filename)
+
+
+
+	return render_template('main.html')
+
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    return redirect(url_for('static',filename = "/Image_Upload" + filename), code=301)
+
+app.run(debug=True,port=2000)
+
+
+
+#images = glob.glob(r"C:\Users\jun63\Downloads\Cropped Science Fair Folder\Hand Soap\25.0_ Hand Soap/*.jpg")
+#img1 = cv2.imread(r"C:\Users\jun63\Downloads\Cropped Science Fair Folder\Distilled Water (Control)/IMG_3819")
+images = ("/Image_Upload" + filename)
+print(images)
+img1 = cv2.imread(images)
+b, g, r = cv2.split(img1)
+
+ttl = img1.size / 3 
+B1 = float(np.sum(b)) / ttl 
+G1 = float(np.sum(g)) / ttl
+R1 = float(np.sum(r)) / ttl
+total = R1+G1+B1
+B = float(np.sum(b)) / ttl / total
+G = float(np.sum(g)) / ttl / total
+R = float(np.sum(r)) / ttl / total
+    
+B_mean1 = list()
+G_mean1 = list()
+R_mean1 = list()
+
+B_mean1.append(B)
+G_mean1.append(G)
+R_mean1.append(R)
+
+print(B_mean1)
+print(G_mean1)
+print(R_mean1)
+
+#create array and feed into the models; make one site for each one????
+
